4b9bba0d345467e54f2aac0d30df1732e15a6792
Jack Nichols <jacknichols@jacks-mbp.home>
2020-09-03 17:26:57 +0100

    finishing sign in handleSubmit method

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import "./SignIn.scss";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import FormInput from "../FormInput/FormInput";
 import CustomBtn from "../CustomBtn/CustomBtn";
@@ -17,8 +17,17 @@ export default class SignIn extends Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
 
     this.setState({ email: "", password: "" });
   };
